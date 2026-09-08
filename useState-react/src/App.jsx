046d126a8150caff6e05d05d030f30ef07a5093d
Md. Mizanur Rahman Khan <032428@jb.com>
2026-09-08 15:53:01 +0600

    useState in more details

--- useState-react/src/App.jsx
+++ useState-react/src/App.jsx
@@ -1,16 +1,19 @@
-import React, { useState } from "react";
+import React, { use, useState } from "react";
 
 const App = () => {
-  const [count, useCount] = useState(0);
+  const [count, setCount] = useState(0);
+  const [user, setUser] = useState({ name: "Mizan", age: 20 }); // object example
+  const [num, setNum] = useState([10, 20, 30]); // array example
+
   return (
-    <div className="h-screen w-screen bg-black">
+    <div className="h-screen w-screen bg-black text-white">
       <div className="h-50 w-50 bg-blue-900 flex justify-center items-center">
         <h1 className="text-white font-bold text-9xl">{count}</h1>
       </div>
       <div className="text-white flex gap-4 pt-6 pl-2">
         <button
           onClick={() => {
-            useCount(count + 1);
+            setCount(count + 1);
           }}
           className="bg-amber-950 p-3 rounded-xl"
         >
@@ -18,7 +21,7 @@ const App = () => {
         </button>
         <button
           onClick={() => {
-            useCount(count - 1);
+            setCount(count - 1);
           }}
           className="bg-amber-950 p-3 rounded-xl"
         >
@@ -26,13 +29,53 @@ const App = () => {
         </button>
         <button
           onClick={() => {
-            useCount(count + 5);
+            setCount(count + 5);
           }}
           className="bg-amber-950 p-3 rounded-xl"
         >
           Add by 5
         </button>
       </div>
+      <div>
+        {user.name}
+        <button
+          onClick={() => {
+            const newUser = { ...user };
+            newUser.name = "Md. Mizanur Rahman Khan";
+            setUser(newUser);
+          }}
+          className="bg-amber-950 p-2 m-2 rounded-xl"
+        >
+          Change User
+        </button>
+      </div>
+      <div>
+        {num}
+        <button
+          onClick={() => {
+            const newNum = [...num];
+            newNum.push(40);
+            setNum(newNum);
+          }}
+          className="bg-amber-950 p-2 m-2 rounded-xl"
+        >
+          Add Num
+        </button>
+      </div>
+      <h1>Adding Multiple set Value</h1>
+      <button
+        onClick={() => {
+          // setCount(count+1) // This method doesn't work
+          // setCount(count+1)
+          // setCount(count+1)
+          setCount(x=>x+1) // we need to use arrow funtion to use multiple set functionality
+          setCount(x=>x+1)
+          setCount(x=>x+1)
+        }}
+        className="bg-amber-950 p-2 m-2 rounded-xl"
+      >
+        3 setCount
+      </button>
     </div>
   );
 };
